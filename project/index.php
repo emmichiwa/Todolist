8414b083ccfd8c049a2209b99c82f87f9e9018f9
Emmelie <emmelie.engstrom@gmail.com>
2018-01-05 01:54:45 +0100

    tried some styling

--- project/index.php
+++ project/index.php
@@ -8,14 +8,12 @@ require 'todolist.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Todo List</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+    <link href="https://fonts.googleapis.com/css?family=Oswald:400,700" rel="stylesheet">
 
-    <link href="https://fonts.googleapis.com/css?family=Courgette|Just+Another+Hand|Rancho|Sriracha" rel="stylesheet">
-    
-    <link rel="stylesheet" href="css/style.css">
-    
+    <title>Todo List</title>    
 </head>
 <body>
     <div class="todowrapper">
