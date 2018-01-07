403c852517eba30ed8090ce2cdb85265a1cfc0e5
Emmelie <emmelie.engstrom@gmail.com>
2018-01-07 20:39:55 +0100

    added footer with github link

--- project/index.php
+++ project/index.php
@@ -17,75 +17,87 @@ require 'todolist.php';
 
 </head>
 <body>
-    <div class="todowrapper">
+    <main>
+        <div class="todowrapper">
 
-        <div class="todolist">
-            <h2>TODO LIST</h2>
+            <div class="todolist">
+                <h2>TODO LIST</h2>
 
-            <form class="add-item" action="index.php" method="POST">
-            <p class="message"><?php echo $message; ?></p>
-                <input type="text"  name="title" placeholder="Add your task here" class="input" autocomplete="off" >
-                <input type="text"  name="name" placeholder="Created By" class="input" autocomplete="off" >
-                <button type="submit" name="submit" class="add_button">Add task</button>
-            </form>
+                <form class="add-item" action="index.php" method="POST">
+                <p class="message"><?php echo $message; ?></p>
+                    <input type="text"  name="title" placeholder="Add your task here" class="input" autocomplete="off" >
+                    <input type="text"  name="name" placeholder="Created By" class="input" autocomplete="off" >
+                    <button type="submit" name="submit" class="add_button">Add task</button>
+                </form>
 
-            <div class="tableTodo">
-                <table>
-                    <thead>
+                <div class="tableTodo">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>All tasks listed</th>
+                                <th>Created By</th>
+                                <th class="thAction">Action</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <?php foreach ($todolist as $todo) {
+                            ?>
+                            <tr>
+                            <td class="task"><p><?= $todo['title']; ?></p></td>
+                            <td class="task"><p><?= $todo['createdBy']; ?></p></td>
+                                <td class="action">
+                                    <a href="index.php?mark_complete=<?php echo $todo['id']; ?>"><i class="fa fa-check" aria-hidden="true"></i></a>
+                                </td>
+                                <td class="action">
+                                    <a href="index.php?delete_task=<?php echo $todo['id']; ?>"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+                                </td>
+                            </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div> <!--tabletodo-->
+            </div> <!--todolist-->
+
+            <div class="doneTasks">
+                <h2>DONE TASKS</h2>
+
+                <div class="tableDone">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Completed Tasks</th>
+                                <th>Created By</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                        <?php foreach ($completedlist as $complete) {
+                            ?>
                         <tr>
-                            <th>All tasks listed</th>
-                            <th>Created By</th>
-                            <th class="thAction">Action</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        <?php foreach ($todolist as $todo) {
-                        ?>
-                        <tr>
-                        <td class="task"><p><?= $todo['title']; ?></p></td>
-                        <td class="task"><p><?= $todo['createdBy']; ?></p></td>
-                            <td class="action">
-                                <a href="index.php?mark_complete=<?php echo $todo['id']; ?>"><i class="fa fa-check" aria-hidden="true"></i></a>
-                            </td>
-                            <td class="action">
-                                <a href="index.php?delete_task=<?php echo $todo['id']; ?>"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+                            <td class="task"><p><?= $complete['title']; ?></p></td>
+                            <td class="task"><p><?= $complete['createdBy']; ?></p></td>
+                            <td class="delete">
+                                <a href="index.php?delete_task=<?php echo $complete['id']; ?>"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
                             </td>
                         </tr>
                         <?php } ?>
-                    </tbody>
-                </table>
-            </div> <!--tabletodo-->
-        </div> <!--todolist-->
+                        </tbody>
+                    </table>
+                </div> <!--tableComplete-->
+            </div> <!--doneTasks-->
+        </div> <!--todowrapper-->
 
-        <div class="doneTasks">
-            <h2>DONE TASKS</h2>
+        <footer>
 
-            <div class="tableDone">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Completed Tasks</th>
-                            <th>Created By</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
+            <div class="socialicons">
+            <h2><a href=#><i class="fa fa-github" aria-hidden="true"></i> EMMICHIWA</a></h2>
+            </div>
 
-                    <tbody>
-                    <?php foreach ($completedlist as $complete) {
-                        ?>
-                    <tr>
-                        <td class="task"><p><?= $complete['title']; ?></p></td>
-                        <td class="task"><p><?= $complete['createdBy']; ?></p></td>
-                        <td class="delete">
-                            <a href="index.php?delete_task=<?php echo $complete['id']; ?>"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
-                        </td>
-                    </tr>
-                    <?php } ?>
-                    </tbody>
-                </table>
-            </div> <!--tableComplete-->
-        </div> <!--doneTasks-->
-    </div> <!--todowrapper-->
+            <div class="clear"></div>
+
+        </footer>
+    </main>
 </body>
 </html>
